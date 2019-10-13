c9b93a1370e9f2ff40ed98512cc8b4bca5566e41
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-10-13 10:08:27 +0700

    Menambahkan pagination

--- application/controllers/Peoples.php
+++ application/controllers/Peoples.php
@@ -6,6 +6,47 @@ class Peoples extends CI_Controller {
     {
         $data['judul'] = 'List of Peoples';
 
+        $this->load->model('Peoples_model', 'peoples');
+
+        $this->load->library('pagination');
+        
+        $config['base_url'] = 'http://localhost/tugas-4-ci-app/peoples/index';
+        $config['total_rows'] = $this->peoples->countAllPeoples();
+        $config['per_page'] = 10;
+        $config['num_links'] = 5;
+
+        $config['full_tag_open'] = '<nav><ul class="pagination justify-content-center">';
+        $config['full_tag_close'] = '</ul></nav>';
+        
+        $config['first_link'] = 'First';
+        $config['first_tag_open'] = '<li class="page-item">';
+        $config['first_tag_close'] = '</li>';
+        
+        $config['last_link'] = 'Last';
+        $config['last_tag_open'] = '<li class="page-item">';
+        $config['last_tag_close'] = '</li>';
+        
+        $config['next_link'] = '&raquo';
+        $config['next_tag_open'] = '<li class="page-item">';
+        $config['next_tag_close'] = '</li>';
+        
+        $config['prev_link'] = '&raquo';
+        $config['prev_tag_open'] = '<li class="page-item">';
+        $config['prev_tag_close'] = '</li>';
+        
+        $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link" href="#">';
+        $config['cur_tag_close'] = '</a></li>';
+        
+        $config['num_tag_open'] = '<li class="page-item">';
+        $config['num_tag_close'] = '</li>';
+
+        $config['attributes'] = array('class' => 'page-link');
+
+        $this->pagination->initialize($config);
+
+        $data['start'] = $this->uri->segment(3);
+        $data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start']);
+
         $this->load->view('templates/header', $data);
         $this->load->view('peoples/index', $data);
         $this->load->view('templates/footer');
--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -2,8 +2,33 @@
     <div class="row">
         <div class="col-md-10">
 
-            <h3>List of Peoples</h3>
+            <h3 class="mt-3">List of Peoples</h3>
 
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach( $peoples as $p ) : ?>
+                        <tr>
+                            <th><?= ++$start; ?></th>
+                            <td><?= $p['name']; ?></td>
+                            <td><?= $p['email']; ?></td>
+                            <td>
+                                <a href="" class="badge badge-warning">detail</a>
+                                <a href="" class="badge badge-success">edit</a>
+                                <a href="" class="badge badge-danger">delete</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            <?= $this->pagination->create_links(); ?>
         </div>
     </div>
 </div>
