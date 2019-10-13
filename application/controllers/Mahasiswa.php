511ee570ec9307ca642215e9eab76c28df665c74
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-10-13 07:22:26 +0700

    Membuat fitur tambah

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -6,6 +6,7 @@ class Mahasiswa extends CI_Controller {
     {
         parent::__construct();
         $this->load->model('Mahasiswa_model');
+        $this->load->library('form_validation');
     }
 
     public function index()
@@ -15,5 +16,22 @@ class Mahasiswa extends CI_Controller {
         $this->load->view('templates/header', $data);
         $this->load->view('mahasiswa/index', $data);
         $this->load->view('templates/footer');
+    }
+
+    public function tambah()
+    {
+        $data['judul'] = 'Form Tambah Data Mahasiswa';
+        
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+        if( $this->form_validation->run() == FALSE) {    
+            $this->load->view('templates/header', $data);
+            $this->load->view('mahasiswa/tambah');
+            $this->load->view('templates/footer');
+        } else {
+
+        }
     }
 }
--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -2,6 +2,12 @@
 
     <div class="row mt-3">
         <div class="col-md-6">
+            <a href="<?= base_url(); ?>mahasiswa/tambah" class="btn btn-primary">Tambah Data Mahasiswa</a>
+        </div>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col-md-6">
             <h3>Daftar Mahasiswa</h3>
             <?php foreach( $mahasiswa as $mhs ) : ?>
                 <ul class="list-group">
